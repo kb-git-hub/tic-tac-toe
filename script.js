8e286d60bd4ebd6dee454a72f06b49294a5024a1
KB <kj.br@protonmail.com>
2022-09-22 15:43:32 -0500

    'Event Delegation'

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 class Gameboard {
     constructor() {
         this.board = []
+        this.drawspeed = 65
     }
 
     // build 9 board squares, apply IDs, push to this.board
@@ -9,7 +10,7 @@ class Gameboard {
             let letter = String.fromCharCode(65 + i)
             for (let j = 1; j <= 3; j++) {
                 const boardSquare = document.createElement('div')
-                const boardSquareID = `${letter}${j} `
+                const boardSquareID = `${letter}${j}`
                 boardSquare.classList.add('boardSquare')
                 boardSquare.setAttribute('id', `${boardSquareID}`)
                 this.board.push(boardSquare)
@@ -19,50 +20,79 @@ class Gameboard {
 
     // append this.board elements to GameBoard at interval
     displayBoard() {
-        let intervalID   
+        let intervalID
         let divID = 0
         const gameBoard = document.querySelector('#gameBoard')
         intervalID = setInterval(() => {
-            gameBoard.append(this.board[divID])
+            gameBoard.appendChild(this.board[divID])
             divID++
             if (divID > 8) clearInterval(intervalID)
-        }, 80)
+        }, this.drawspeed)
     }
-
     // Place Piece
-    placePiece(){}
-
+    placePiece() { }
     //Reset Board
-    resetBoard(){}
+    resetBoard() { }
+}
+
+
+
+class Player {
+    constructor(piece = 'X', type = 'human') {
+        this.piece = piece
+        this.type = type
+        this.wins = 0
+    }
+    displayWins() { }
 }
 
 
 
 
-
-class Player {
-    constructor(piece = 'X', type = 'human'){
-        this.piece = piece
-        this.type = type
-        this.wins = 0
+class GameLogic {
+    constructor(p1 = 'player', p2 = 'player') {
+        this.p1 = p1
+        this.p2 = p2
     }
-    updateWins(){}
- }
-
-
-const player1 = new Player()
-
- class GameLogic { }
-
-
+    verifyGameType() { }
+    createPlayers() { }
+    startGame() { }
+    resetGame() { }
+    checkEmptySquare() { }
+    checkforWin() { }
+    updateWinner() { }
+}
 
 
 // game init
+const player1 = new Player()
 const board = new Gameboard()
+const game = new GameLogic()
 board.buildBoard()
 board.displayBoard()
 
 
 
+// User interface
+const
+    gameBoard = document.querySelector('#gameBoard'),
+    startButton = document.querySelector('#bn-new-game'),
+    resetButton = document.querySelector('#bn-reset-game'),
 
+    gameTypeSelectorPvP = document.querySelector('#btn-pvp'), 
+    gameTypeSelectorPvAI = document.querySelector('#btn-pvc'),
 
+    p1Score = document.querySelector('#p1Score'),
+    p2Score = document.querySelector('#p2Score')
+
+    
+/*
+Event Listeners
+*/
+
+//Event Delegator for Dynamic boardSquares
+const handleBoardClick = e => {
+    const clickedSquare = e.target.getAttribute('id')
+    if (clickedSquare !== 'gameBoard'){console.log(clickedSquare)}
+}
+gameBoard.addEventListener('click', handleBoardClick)
