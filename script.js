4eb048395ee052af09af70d705347ba39a8fbadf
KB <kj.br@protonmail.com>
2022-09-22 11:45:57 -0500

    Update js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ class Gameboard {
         this.board = []
     }
 
+    // build 9 board squares, apply IDs, push to this.board
     buildBoard() {
-        // build 9 board squares, apply IDs, push to this.board
         for (let i = 0; i < 3; i++) {
             let letter = String.fromCharCode(65 + i)
             for (let j = 1; j <= 3; j++) {
@@ -15,28 +15,52 @@ class Gameboard {
                 this.board.push(boardSquare)
             }
         }
-        // append this.board elements to GameBoard at interval
-        let intervalID
+    }
+
+    // append this.board elements to GameBoard at interval
+    displayBoard() {
+        let intervalID   
         let divID = 0
         const gameBoard = document.querySelector('#gameBoard')
         intervalID = setInterval(() => {
             gameBoard.append(this.board[divID])
             divID++
             if (divID > 8) clearInterval(intervalID)
-        }, 95)
+        }, 80)
     }
 
+    // Place Piece
+    placePiece(){}
+
+    //Reset Board
+    resetBoard(){}
 }
 
 
 
 
-class GameLogic { }
-class Player { }
 
+class Player {
+    constructor(piece = 'X', type = 'human'){
+        this.piece = piece
+        this.type = type
+        this.wins = 0
+    }
+    updateWins(){}
+ }
+
+
+const player1 = new Player()
+
+ class GameLogic { }
+
+
+
+
+// game init
 const board = new Gameboard()
-
 board.buildBoard()
+board.displayBoard()
 
 
 
